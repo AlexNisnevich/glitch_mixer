dd1b4218b12983f6c0dea0f002b56ec3f31b83c3
Alex Nisnevich <alex.nisnevich@gmail.com>
2013-02-21 02:27:41 -0800

    adding interpreter, new commands (quit, exec)

--- glitch_mixer.py
+++ glitch_mixer.py
@@ -15,6 +15,44 @@ import signal
 
 FNULL = open(os.devnull, 'w')
 
+def run(command):
+	parts = command.split(' ')
+	func = parts[0]
+
+	if func == '#':
+		# comment
+		pass
+
+	elif func == 'start':
+		# play specified oneliner
+		name = parts[1]
+		process = sub.Popen('%s/_temp_%s' % (os.getcwd(), name), stdout=sub.PIPE)
+		aplay_process = sub.Popen('aplay -q', stdin=process.stdout, shell=True, preexec_fn=os.setsid)
+		running.setdefault(name, []).append((process, aplay_process))
+
+	elif func == 'stop':
+		# stop specified oneliner
+		name = parts[1]
+		processes = running[name].pop()
+		processes[0].terminate() # kill program
+		os.killpg(processes[1].pid, signal.SIGTERM) # kill aplay (running in spawned shell)
+
+	elif func == 'sleep':
+		# delay X secs
+		duration = parts[1]
+		time.sleep(float(duration))
+
+	elif func == 'exec':
+		# execute mixfile
+		mixfile = parts[1]
+		for line in open(mixfile):
+			line = line.strip()
+			print line
+			run(line)
+
+	else:
+		print 'Unknown command: %s' % func
+
 library = {
 	'alex1': "t>>4|t>>2|t+t/2", # this one's my own
 	'tejeez1': "(t*(t>>5|t>>8))>>(t>16)", # credit to tejeez (from viznut's 1st video)
@@ -23,59 +61,26 @@ library = {
 
 running = {}
 
-# simple program to test start/stop/sleep functionality
-commands = [
-	'start alex1',
-	'sleep 4',
-	'start tejeez1',
-	'sleep 3',
-	'stop alex1',
-	'sleep 5',
-	'stop tejeez1',
-	'start alex1',
-	'start viznut1',
-	'sleep 8',
-	'start alex1',
-	'sleep 3',
-	'start alex1',
-	'sleep 3',
-	'start alex1',
-	'sleep 3',
-	'start alex1',
-	'sleep 2'
-]
-
 for key, oneliner in library.iteritems():
 	program = "main(t){for(t=0;;t++)putchar(%s);}" % oneliner
-	os.system('echo "%s" > temp.c' % program)
-	os.system('gcc temp.c -o temp_%s' % key)
+	os.system('echo "%s" > _temp.c' % program)
+	os.system('gcc _temp.c -o _temp_%s' % key)
 
-try:
-	for command in commands:
-		print command
-		parts = command.split(' ')
-		func = parts[0]
+while True:
+	try:
+		command = raw_input('>')
+		if command == 'exit' or command == 'quit':
+			break
+		else:
+			run(command)
+	except Exception:
+		traceback.print_exc()
+	except KeyboardInterrupt:
+		print
+		break
 
-		if func == 'start':
-			# play specified oneliner
-			name = parts[1]
-			process = sub.Popen('%s/temp_%s' % (os.getcwd(), name), stdout=sub.PIPE)
-			aplay_process = sub.Popen('aplay -q', stdin=process.stdout, shell=True, preexec_fn=os.setsid)
-			running.setdefault(name, []).append((process, aplay_process))
-		elif func == 'stop':
-			# stop specified oneliner
-			name = parts[1]
-			processes = running[name].pop()
-			processes[0].terminate() # kill program
-			os.killpg(processes[1].pid, signal.SIGTERM) # kill aplay (running in spawned shell)
-		elif func == 'sleep':
-			# delay X secs
-			duration = parts[1]
-			time.sleep(float(duration))
-except Exception:
-	traceback.print_exc()
-
-os.system('killall aplay > /dev/null')
+print 'Cleaning up ...'
+os.system('killall aplay > /dev/null 2>&1')
 for key in library.keys():
-	os.system('killall temp_%s > /dev/null' % key)
-os.system('rm temp*')
+	os.system('killall _temp_%s > /dev/null 2>&1' % key)
+os.system('rm _temp* > /dev/null 2>&1')
